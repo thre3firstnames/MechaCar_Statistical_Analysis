8e2985e94913eee262498bbb185ba0625fab4a00
thre3firstnames <natkmerry@gmail.com>
2022-04-03 18:25:45 -0600

    Deliv3 coding complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -20,3 +20,13 @@ total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI
 
 # create lot_summary frame and group by Lot#
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') 
+
+
+#Deliv3
+#test PSI across all Manufacturing lots with the population mean
+t.test(suspension_coil$PSI,mu=1500)
+
+#test PSI across each Manufacturing lot
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot1")[['PSI']],mu=1500)
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot2")[['PSI']],mu=1500)
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot3")[['PSI']],mu=1500)
